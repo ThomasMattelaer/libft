cca51ba23ffa4eef1120953c1bf12aa3cbe5702c
Thomas Mattelaer <tmattela@shi-r5-p4.s19.be>
2026-04-08 16:32:14 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,15 +14,28 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	start;
+	size_t	end;
 	size_t	i;
-	size_t	j;
 	char	*strim;
 
 	if (!s1 || !set)
 		return (NULL);
-	strim = malloc(sizeof(char) * (ft_strlen(s1) - ft_strlen(set) + 1));
+	start = 0;
+	while (ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[end]))
+		end--;
+	strim = malloc(sizeof(char) * (end - start + 2));
 	if (!strim)
 		return (NULL);
-	ft_strnstr(s1, set, ft_strlen(set));
-	
+	i = 0;
+	while ((end - start) > i)
+	{
+		strim[i] = s1[start + i];
+		i++;
+	}
+	strim[i] = '\0';
+	return (strim);
 }
