db62a13070e540301fc47db34178d17a761b53e5
Thomas Mattelaer <tmattela@shi-r7-p6.s19.be>
2026-04-09 11:47:42 +0200

    ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmattela <tmattela@student.42belgium.com>  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-04-09 08:29:05 by tmattela          #+#    #+#             */
+/*   Updated: 2026-04-09 08:29:05 by tmattela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int int_len(long nb)
+{
+	int	count;
+
+	count = 0;
+	if(nb < 0)
+	{
+		count++;
+		nb *= -1;
+	}
+	if (nb == 0)
+		count++;
+	while (nb != 0)
+	{
+		nb /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	long	nb;
+	char	*str;
+	int		len;
+	int		i;
+
+	nb = n;
+	len = int_len(nb);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = len - 1;
+	if (n < 0)
+		str[0] = '-';
+	str[len] = 0;
+	while(nb != 0)
+	{
+		str[i] = (nb % 10) + '0';
+		nb /= 10;
+		i--;
+	}
+}
